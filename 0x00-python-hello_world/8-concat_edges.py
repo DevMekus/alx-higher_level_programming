ea7186a47daf060d00c001c83efb5b8703070f31
DevMekus <nnaji.nnaemeka712@gmail.com>
2023-11-27 10:56:12 -0800

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str.replace(str,"object-oriented programming with Python")
-str = f"{str}\n"
+         language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[115:120] + str[0:6]
 print(str)
