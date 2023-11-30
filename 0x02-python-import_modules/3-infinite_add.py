c0f4c5732cd2d8b4cc3d858b74397134f6f6bf6a
DevMekus <nnaji.nnaemeka712@gmail.com>
2023-11-30 04:29:31 -0800

    correction 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ import math
 
 if __name__ == "__main__":
     result = 0
-    arguments =sys.argv[1:]
+    arguments = sys.argv[1:]
     for i in arguments:
         result += int(i)
     print("{}".format(result))
