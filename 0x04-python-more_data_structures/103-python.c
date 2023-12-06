5d03e9691d99a0912119cef5a5051bcd4849bb74
DevMekus <nnaji.nnaemeka712@gmail.com>
2023-12-06 11:17:21 -0800

    correction2

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -7,7 +7,7 @@ void print_python_bytes(PyObject *p)
 {
 	long int size;
 	int i;
-	char *trying_str = NULL;
+	char *t_str = NULL;
 
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
@@ -16,16 +16,16 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	PyBytes_AsStringAndSize(p, &trying_str, &size);
+	PyBytes_AsStringAndSize(p, &t_str, &size);
 
 	printf("  size: %li\n", size);
-	printf("  trying string: %s\n", trying_str);
+	printf("  trying string: %s\n", t_str);
 	if (size < 10)
 		printf("  first %li bytes:", size + 1);
 	else
 		printf("  first 10 bytes:");
 	for (i = 0; i <= size && i < 10; i++)
-		printf(" %02hhx", trying_str[i]);
+		printf(" %02hhx", t_str[i]);
 	printf("\n");
 }
 
@@ -33,18 +33,17 @@ void print_python_list(PyObject *p)
 {
         long int size = PyList_Size(p);
         int i;
-        PyListObject *list = (PyListObject *)p;
+        PyListObject *list_ad = (PyListObject *)p;
         const char *type;
 
         printf("[*] Python list info\n");
         printf("[*] Size of the Python List = %li\n", size);
-        printf("[*] Allocated = %li\n", list->allocated);
+        printf("[*] Allocated = %li\n", list_ad->allocated);
         for (i = 0; i < size; i++)
         {
-                type = (list->ob_item[i])->ob_type->tp_name;
+                type = (list_ad->ob_item[i])->ob_type->tp_name;
 		printf("Element %i: %s\n", i, type);
                 if (!strcmp(type, "bytes"))
-                        print_python_bytes(list->ob_item[i]);
+                        print_python_bytes(list_ad->ob_item[i]);
         }
 }
-
