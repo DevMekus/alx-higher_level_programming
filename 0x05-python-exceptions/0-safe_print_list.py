2ea3d89ce2a03ec0bae300747793735e85337677
DevMekus <nnaji.nnaemeka712@gmail.com>
2023-12-18 06:21:30 -0800

    corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for index in range(x):
         try:
             print(my_list[index], end="")
-            number++
+            number += 1
         except IndexError:
             break
     print("")
