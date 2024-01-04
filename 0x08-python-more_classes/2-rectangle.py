84c8f358069dd9e2492da92f7d8ca6ec28e6c87c
DevMekus <nnaji.nnaemeka712@gmail.com>
2024-01-04 12:47:42 -0800

    Corrections

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
             Raise:
                 Raises a TypeError if value is not integer
         """
-        if not type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -45,7 +45,7 @@ class Rectangle:
             Raise:
                 Raises a TypeError if value is not integer
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
