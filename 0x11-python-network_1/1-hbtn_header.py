c9489adee15c6cf84401cde8c8ca3f38c09d925a
DevMekus <nnaji.nnaemeka712@gmail.com>
2024-03-28 17:29:21 +0100

    Change import in task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+import sys
+import urllib.request
 """
 Displays the value of the X-Request-Id variable
 """
 
-if __name__ == "__main__":
-    import sys
-    import urllib.request
+if __name__ == "__main__":  
 
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.headers["X-Request-Id"])
