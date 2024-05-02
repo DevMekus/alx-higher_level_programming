84c0d1c4c74ca2fd30b17585456ac07d4fd49562
DevMekus <nnaji.nnaemeka712@gmail.com>
2024-05-02 15:11:05 +0100

    JTask

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+// Function Queries an API and fetches all movie
+
+let url = "https://swapi.co/api/films/?format=json";
+$.get(url, function (data) {
+  let films = data.results;
+  for (let film of films) {
+    $("ul#list_movies").append(`<li>${film.title}</li>`);
+  }
+});
